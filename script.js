4f1a9e4e2ff6747430a6f9d54281871647cb17a8
Abhilash Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2026-01-22 23:03:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ let form=document.querySelector('form');
 form.addEventListener('submit',function(e){
 	e.preventDefault();
 
-    let name=document.getElementhttps://www.svgrepo.com/show/345221/three-dots.svgById("name").value.trim();
+    let name=document.getElementById("name").value.trim();
 
     let age=document.getElementById("age").value.trim();
 
-	https://www.svgrepo.com/show/345221/three-dots.svg
 	if(name=="" || age==""){
 		alert("Please enter valid details.");
 
