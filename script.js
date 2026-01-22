8f71a2aede56c64b8cdd9037801c5366ac4ce180
Abhilash Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2026-01-22 22:34:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+
+
+let form=document.querySelector('form');
+
+form.addEventListener('submit',function(e){
+	e.preventDefault();
+
+    let name=document.getElementById("name").value.trim();
+
+    let age=document.getElementById("age").value.trim();
+
+	
+	if(name=="" || age==""){
+		alert("Please enter valid details.");
+
+		return;
+	}
+
+	// Call Promise. Because access the data of age and name.
+
+	youVote(age,name)
+	.then((res)=>{
+		document.write(res);
+	})
+    .catch((err)=>{
+		document.write(err);
+	})
+})
+
+function youVote(age,name){
+	return new Promise((resolve,reject)=>{
+		if(age>=18){
+			setTimeout(()=>{
+				resolve(`Welcome, ${name}. You can vote.`);
+			},4000);
+			
+		}else{
+			setTimeout(()=>{
+				reject(`Oh sorry ${name}. You aren't old enough.`);
+			},4000);
+			
+		}
+	})
+}
